01468d08fef0b895aea985019db0294e041e0756
Luigi Ilie Aron <aronluigi@gmail.com>
2014-05-01 00:10:07 +0200

    fix frame add export examples

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 from tkinter import *
-from GUI import frame
+from GUI import Frame
 
 root = Tk()
 root.title("Test Python Luigi")
 root.geometry("1024x768")
 
-app = frame.Application(root)
+app = Frame.Application(root)
 
 root.mainloop()
